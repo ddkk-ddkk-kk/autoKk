b736318a998ac2daaef7ca22127316f279deee7a
ddkk-ddkk-kk <kdomain249@gmail.com>
2026-09-06 20:53:13 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,4 @@
-import sys
-import os
-
-# Add parent directory to path
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-# Import your main app
 from autoshopify_api_v3 import app
 
-# Vercel handler
+# Vercel needs this
 handler = app
